c91f40d4356c6d7819ac2288d8bbd69ffc440125
HammadSiddique <hammad.siddique01@gmail.com>
2022-05-22 00:47:49 +0500

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-import "./style.css";
+import './style.css';
 
-const tableScores = document.querySelector(".table-scores");
-const submitScore = document.querySelector(".submit-game");
-const name = document.querySelector(".name-input");
-const playerScore = document.querySelector(".score-input");
-const refresh = document.querySelector(".refresh-btn");
-const url =
-  "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/IPWYQeHR5U8y45jrsCzj/scores/";
+const tableScores = document.querySelector('.table-scores');
+const submitScore = document.querySelector('.submit-game');
+const name = document.querySelector('.name-input');
+const playerScore = document.querySelector('.score-input');
+const refresh = document.querySelector('.refresh-btn');
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/IPWYQeHR5U8y45jrsCzj/scores/';
 
 const renderLeaderboard = (users) => {
   const user = users.result;
-  let leaderboard = "";
+  let leaderboard = '';
   user.forEach((gameUser) => {
     leaderboard += `<li class='scores'>${gameUser.user}: ${gameUser.score}</li>`;
     tableScores.innerHTML = leaderboard;
@@ -23,15 +22,15 @@ const getScores = async () => {
   renderLeaderboard(data);
 };
 
-refresh.addEventListener("click", () => {
+refresh.addEventListener('click', () => {
   getScores();
 });
 
 const scoreForm = async () => {
   const response = fetch(url, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify({
       user: name.value,
@@ -39,12 +38,12 @@ const scoreForm = async () => {
     }),
   });
   const data = await (await response).json();
-  name.value = "";
-  playerScore.value = "";
+  name.value = '';
+  playerScore.value = '';
   return data;
 };
 
-submitScore.addEventListener("submit", (e) => {
+submitScore.addEventListener('submit', (e) => {
   e.preventDefault();
   scoreForm();
 });
