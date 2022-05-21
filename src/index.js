3664edc36d34d2d53b1b0076e91a0f6786967514
HammadSiddique <hammad.siddique01@gmail.com>
2022-05-21 23:29:54 +0500

    Use API fro leaderboard

--- src/index.js
+++ src/index.js
@@ -1,1 +1,49 @@
 import './style.css';
+
+const tableScores = document.querySelector('.table-scores');
+const submitScore = document.querySelector('.submit-game');
+const name = document.querySelector('.name-input');
+const playerScore = document.querySelector('.score-input');
+const refresh = document.querySelector('.refresh-btn');
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/IPWYQeHR5U8y45jrsCzh/scores/';
+
+const renderLeaderboard = (users) => {
+  const user = users.result;
+  let leaderboard = '';
+  user.forEach((gameUser) => {
+    leaderboard += `<li class='scores'>${gameUser.user}: ${gameUser.score}</li>`;
+    tableScores.innerHTML = leaderboard;
+  });
+};
+
+async function getScores() {
+  const response = await fetch(url);
+  const data = await response.json();
+  renderLeaderboard(data);
+}
+
+refresh.addEventListener('click', () => {
+  getScores();
+});
+
+async function scoreForm() {
+  const response = fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      user: name.value,
+      score: playerScore.value,
+    }),
+  });
+  const data = await (await response).json();
+  name.value = '';
+  playerScore.value = '';
+  return data;
+}
+
+submitScore.addEventListener('submit', (e) => {
+  e.preventDefault();
+  scoreForm();
+});
